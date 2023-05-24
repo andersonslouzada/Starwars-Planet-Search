c35323a1180e17f08cc2926c3e0ab472cc0501f4
andersonlouzada <anderson.s.louzada@gmail.com>
2023-05-24 15:11:29 -0300

    cumpre requisito 4

--- src/components/PlanetsTable.js
+++ src/components/PlanetsTable.js
@@ -9,12 +9,36 @@ export default function PlanetsTable() {
     value: '0',
   });
   const [filteredPlanets, setFilteredPlanets] = useState([]);
+  const [appliedFilters, setAppliedFilters] = useState([]);
 
   useEffect(() => {
-    const filteredData = planets.filter((planet) => planet.name
+    let filteredData = planets;
+
+    appliedFilters.forEach((filter) => {
+      if (filter.column && filter.comparison && filter.value !== '') {
+        const filterNumber = parseFloat(filter.value);
+
+        if (filter.comparison === 'maior que') {
+          filteredData = filteredData.filter(
+            (planet) => parseFloat(planet[filter.column]) > filterNumber,
+          );
+        } else if (filter.comparison === 'menor que') {
+          filteredData = filteredData.filter(
+            (planet) => parseFloat(planet[filter.column]) < filterNumber,
+          );
+        } else if (filter.comparison === 'igual a') {
+          filteredData = filteredData.filter(
+            (planet) => parseFloat(planet[filter.column]) === filterNumber,
+          );
+        }
+      }
+    });
+
+    filteredData = filteredData.filter((planet) => planet.name
       .toLowerCase().includes(nameFilter.toLowerCase()));
+
     setFilteredPlanets(filteredData);
-  }, [planets, nameFilter]);
+  }, [planets, nameFilter, appliedFilters]);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -27,32 +51,22 @@ export default function PlanetsTable() {
   const handleNameFilterChange = (event) => {
     const { value } = event.target;
     setNameFilter(value);
-    setFilteredPlanets(planets.filter((planet) => planet.name
-      .toLowerCase().includes(value.toLowerCase())));
   };
 
   const handleFilterClick = () => {
-    let filteredData = planets;
+    const newFilter = {
+      column: filters.column,
+      comparison: filters.comparison,
+      value: filters.value,
+    };
 
-    if (filters.column && filters.comparison && filters.value !== '') {
-      const filterNumber = parseFloat(filters.value);
+    setAppliedFilters((prevFilters) => [...prevFilters, newFilter]);
+  };
 
-      if (filters.comparison === 'maior que') {
-        filteredData = filteredData.filter(
-          (planet) => parseFloat(planet[filters.column]) > filterNumber,
-        );
-      } else if (filters.comparison === 'menor que') {
-        filteredData = filteredData.filter(
-          (planet) => parseFloat(planet[filters.column]) < filterNumber,
-        );
-      } else if (filters.comparison === 'igual a') {
-        filteredData = filteredData.filter(
-          (planet) => parseFloat(planet[filters.column]) === filterNumber,
-        );
-      }
-    }
-
-    setFilteredPlanets(filteredData);
+  const removeFilter = (index) => {
+    const updatedFilters = [...appliedFilters];
+    updatedFilters.splice(index, 1);
+    setAppliedFilters(updatedFilters);
   };
 
   return (
@@ -98,6 +112,14 @@ export default function PlanetsTable() {
       <button data-testid="button-filter" onClick={ handleFilterClick }>
         Filter
       </button>
+      <div>
+        {appliedFilters.map((filter, index) => (
+          <div key={ index }>
+            <span>{`${filter.column} ${filter.comparison} ${filter.value}`}</span>
+            <button onClick={ () => removeFilter(index) }>Remover</button>
+          </div>
+        ))}
+      </div>
       <table>
         <thead>
           <tr>
